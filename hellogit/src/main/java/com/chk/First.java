f3fdc27602722b118265be466bf443df525ffd76
CHK <2398865146@qq.com>
2021-08-01 10:55:17 +0800

    更新1

--- hellogit/src/main/java/com/chk/First.java
+++ hellogit/src/main/java/com/chk/First.java
@@ -2,6 +2,7 @@ package com.chk;
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("1");
+        //System.out.println("1");
+        System.out.println("更新1");
     }
 }
