b4f2413dc66a7ef491ba8e1cff8ff7e31ceb250c
CHK <2398865146@qq.com>
2021-08-01 11:07:05 +0800

    分支提交1

--- hellogit/src/main/java/com/chk/First.java
+++ hellogit/src/main/java/com/chk/First.java
@@ -4,5 +4,6 @@ public class First {
     public static void main(String[] args) {
         //System.out.println("1");
         System.out.println("更新1");
+        System.out.println("分支dev的1");
     }
 }
